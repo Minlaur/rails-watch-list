04d7c3af700b2b45bce1473616dd36c076e8efb0
Laurice Minamihara <lauricejoie.m@gmail.com>
2024-01-13 16:01:37 +0900

    complete routing, controller, and views for lists and bookmarks

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,20 @@
+class BookmarksController < ApplicationController
+before_action :set_list, only: %i[new create]
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new
+    @bookmark.save
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
