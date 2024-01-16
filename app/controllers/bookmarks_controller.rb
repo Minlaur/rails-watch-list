b12498f56046d77dfe38693d195b78d76d310cc6
Laurice Minamihara <lauricejoie.m@gmail.com>
2024-01-16 21:02:28 +0900

    updated everything

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,18 @@
 class BookmarksController < ApplicationController
-before_action :set_list, only: %i[new create]
+  before_action :set_list, only: %i[new create]
+
   def new
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
     if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -16,5 +22,9 @@ before_action :set_list, only: %i[new create]
 
   def set_list
     @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'lists#index'
+  # updated so that the root will go to the index
+
   resources :lists do
     resources :bookmarks, only: [ :new, :create, :destroy]
   end
